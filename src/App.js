2e2b902090340725ca634dd409c18dcd6fb5ceea
macbook <mai.le.duong@scancom.net>
2021-09-18 15:57:48 +0700

    new branch

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello React</h1>
+        <h2>new branch develop</h2>
       </header>
     </div>
   );
